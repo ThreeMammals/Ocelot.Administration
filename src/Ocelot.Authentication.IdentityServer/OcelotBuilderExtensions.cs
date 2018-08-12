8801ff971784b501e37bafe6b4a43a5e4cd7754b
Tom Pallister <tom@threemammals.com>
2018-08-12 16:37:11 +0100

    initial from archetype

---
+++ src/Ocelot.Authentication.IdentityServer/OcelotBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Ocelot.Authentication.IdentityServer
+{
+    using DependencyInjection;
+
+    public static class OcelotBuilderExtensions
+    {
+        public static IOcelotBuilder AddSomething(this IOcelotBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
